be63a84e2674ea785896733a669074506a65c6e9
gabacame <ga.bacame@gmail.com>
2024-03-04 12:09:30 -0700

    base case

---
+++ main.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+import os
+
+llm = OpenAI(
+    # place your OpenAI key in an environment variable
+    #api_key=os.environ['OPENAI_API_KEY'] # this is the default
+    base_url="http://localhost:1234/v1"  # see chapter 1 video 3
+)
+
+system_prompt = """Given the following short description
+    of a particular topic, write 3 attention-grabbing headlines 
+    for a blog post. Reply with only the titles, one on each line,
+    with no additional text.
+    DESCRIPTION:
+"""
+user_input = """AI Orchestration with LangChain and LlamaIndex
+    keywords: Generative AI, applications, LLM, chatbot"""
+
+response = llm.chat.completions.create(
+    model="gemma-2b",
+    max_tokens=500,
+    temperature=0.7,
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_input}
+    ]
+)
+
+print(response.choices[0].message.content)
+
+
